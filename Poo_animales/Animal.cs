2a3924ac5970277f8fa3d560d123a1e48371d42c
alex-vinicio <alexandino2011@gmail.com>
2021-09-13 00:08:13 -0500

    creacion de clase animal

---
+++ Poo_animales/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_animales
+{
+    class Animal
+    {
+        public string nombreA;
+        public int edadA;
+
+        public string caminarAnimal()
+        {
+            return "Estoy caminando";
+        }
+
+        public string comerAniaml()
+        {
+            return "Estoy comiendo";
+        }
+    }
+}
